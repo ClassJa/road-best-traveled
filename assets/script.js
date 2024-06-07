146ae28dc0a364b9b96f9e36bc9283c9873cd49e
JA <69608172+ClassJay@users.noreply.github.com>
2024-06-06 20:53:25 -0500

    Working on issue 8

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,33 @@
 let keyword = prompt("Enter your desired agency")
 
+const APIKey = "f+m7/YyogybRKvTgVDkyYCL5ScgVxx8KL49/DEOPiJE="
+// const selects = $("#selects");
+// const selectsTwo =$("#selectsTwo")
+// const searchInput = $('#search-input');
+// const fetchButton = $('button')
+
 keyword.toLowerCase()
 
-// const require(request)
+function getApi() {
+    const requestUrl = `https://developer.usajobs.gov/codelist/agencysubelements/Search?q=${keyword}`;
+  
+    fetch(requestUrl, {
+        method: "GET",
+        headers: {                  
+            "Authorization-Key": APIKey      
+        }}
+    )
+      .then(function (response) {
+        if (!ok) {
+            throw new Error(`HTTP error! Status: ${Response.status}`);
+        }
+        return response.json();
 
-fetch(`https://developer.usajobs.gov/tutorials/search-jobs?q=${keyword}`)
+      })
 
-.then((Response) => {
-    if (!ok) {
-        throw new Error(`HTTP error! Status: ${Response.status}`);
-    }
-    return Response.json()
-})
-
-// .then((data) => {
-
-
-// })
+      .then((data) => {
+        console.log(data);
+        return data
+    
+      });
+  }
