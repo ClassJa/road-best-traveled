587018795a1041a17e2f1b53571eece5cc30fdd5
Kelsey Frahm <k.rae.heider@gmail.com>
2024-06-06 21:27:37 -0500

    adding logic for drop down pull

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,35 @@
 const APIKey = "f+m7/YyogybRKvTgVDkyYCL5ScgVxx8KL49/DEOPiJE="
-const selects = $("#selects");
-const selectsTwo =$("#selectsTwo")
-const searchInput = $('#search-input');
-const fetchButton = $('button')
+const selects = document.getElementById("selects");
+const searchInput = document.getElementById('search-input');
+// const fetchButton = document.getElementB('button')
 
-function getApi() {
-    const requestUrl = 'https://data.usajobs.gov/api/codelist/agencysubelements';
-  
-    fetch(requestUrl, {
+const agencyUrl = 'https://data.usajobs.gov/api/codelist/agencysubelements';
+const agencyList = [];
+let agencyAttributes = {code: [], agency: []}
+
+const fetchAgency = agencyUrl => {
+fetch(agencyUrl, {
         method: "GET",
         headers: {                  
             "Authorization-Key": APIKey      
         }}
     )
+    
       .then(function (response) {
         return response.json();
       })
 
       .then(function (data) {
         console.log(data);
-    
-      });
-  }
+        agencyList.push(data)
+        return(agencyList)
+      }
+
+      .then(function (agencies) {
+        agencies.array.forEach(agency => {agencyList.push(agency)
+          renderAgencyList(agencyList)
+        });
+      })
+      
+   );
+     }
