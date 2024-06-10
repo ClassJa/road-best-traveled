86f331c9464b489b4b0e0f93688fa1c62534a2f6
Kelsey Frahm <k.rae.heider@gmail.com>
2024-06-10 13:51:58 -0500

    dropdowns now working for both fields

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,11 @@ const APIKey = "f+m7/YyogybRKvTgVDkyYCL5ScgVxx8KL49/DEOPiJE="
 const agencyDrop = document.getElementById("agencyDrop");
 const searchInput = document.getElementById('search-input');
 const fetchButton = document.getElementById('button')
+const typeDrop = document.getElementById('typeDrop')
 
 const agencyUrl = 'https://data.usajobs.gov/api/codelist/agencysubelements';
 const agencyList = [];
+const jobType = ['Full-time', 'Part-time', 'Shift Work', 'Intermittent','Job sharing','Multiple'];
 
 //keyword.toLowerCase
 
@@ -32,17 +34,31 @@ function getDropDown() {
 
       .then(function (data) {
         
-        for (let i = 0; i < agencyList.length; i++) {
-        const agency = agencyList[i]
+        for (let i = 0; i < agencyList[0].length; i++) {
+        const agency = agencyList[0][i]
 
         const div = document.createElement('option');
        
-        div.setAttribute("agencyID", agency[i].Code)
-        div.textContent = agency[i].Value
+        div.setAttribute("agencyID", agency.Code)
+        div.textContent = agency.Value
        
         agencyDrop.appendChild(div)
         
       }})
+
+      function jobDrop (){
+        console.log(jobType)
+        for (let i = 0; i < jobType.length; i++) {
+          const type = jobType[i]
+  
+          const div = document.createElement('option');
+         
+          div.setAttribute("job", i+1)
+          div.textContent = type
+         
+          typeDrop.appendChild(div)
+      }}
+      jobDrop()
 }
 
 function getApi() {
