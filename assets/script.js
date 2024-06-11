271a41f89d7303fcf59525d5b0ea4b3b6a32807c
JA <69608172+ClassJay@users.noreply.github.com>
2024-06-10 20:16:37 -0500

    Working to add logic so the modal pops up only when the submit button is clicked

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,17 @@
 const modalPopUp = document.querySelector('#label')
+const modal = document.querySelector('modal-pop-up')
 
 const submit = document.querySelector('submit-btn')
 
-submit.addEventListener('click', )
+submit.addEventListener('click', showModal)
+
+
+function showModal(event) {
+  event.preventDefault
+  if (submit.getAttribute('visibility') === 'hidden') {
+    modal.setAttribute('visibility', 'visible')
+  } else {
+    console.log('')
+    // modal.setAttribute('visibility', 'hidden')
+  }
+}
